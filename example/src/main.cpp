2c059adada57dd987adbef919452546867b6f1f5
Joe <joe@showblender.com>
2016-04-24 09:57:27 -0700

    Update to work with of 0.9.3
    Updated architecture and build settings

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,35 +1,13 @@
 #include "ofMain.h"
-#include "ofxMyIP.h"
-
-class ofApp : public ofBaseApp{
-private:
-    ofxMyIP myip;
-    string result;
-public:
-    void setup(){
-        ofBackground(0);
-        ofSetFrameRate(30);
-        myip.setup();
-        
-        vector<ofxMyIPAddress> list = myip.getList();
-        stringstream s;
-        for (vector<ofxMyIPAddress>::iterator o = list.begin(); o != list.end(); o++) {
-            s << "interface: " << o->name << ", ip: " << o->address << ", netmask: " << (!o->netmask.empty() ? o->netmask : "null") << ", broadcast address: " << (!o->broadcast.empty() ? o->broadcast : "null") << endl;
-        }
-        result = s.str();
-    }
-    void update(){
-        
-    }
-    void draw(){
-        ofDrawBitmapString(result, 0, 10);
-    }
-    
-};
+#include "ofApp.h"
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);
+	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
 	ofRunApp(new ofApp());
-    
+
 }
